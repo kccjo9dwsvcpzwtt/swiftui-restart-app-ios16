d641a0cbc97cd9552db8a61be39695405f814268
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2023-12-01 16:02:37 +0200

    Initial Commit

---
+++ swiftui-restart-app-ios16/swiftui_restart_app_ios16App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_restart_app_ios16App.swift
+//  swiftui-restart-app-ios16
+//
+//  Created by Stanislav Ponomarenko on 2023-12-01.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_restart_app_ios16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
